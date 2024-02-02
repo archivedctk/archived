928f01ae1a952ca39e0b2033f5dcd47893f9d3ea
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2024-02-02 21:36:46 +0900

    add

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,16 @@ import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
 
+import nodejs from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://荒らし.com',
 	integrations: [mdx(), sitemap()],
 	output: "server",
+	adapter: nodejs({
+		mode: "standalone",
+	}),
 	server: {
 		port: 1919
 	},
